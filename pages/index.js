333663f3d9d109cc27ecffb45be1251ecd6f682c
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2023-08-07 16:26:51 +0000

    frase para Henrique, Eliseu2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home() {
     return 
     <>
     
-    <h1>Tenho muito orgulho de você, Henrique Anes Altgauzen!
+    <h1>Tenho muito orgulho de você, Henrique Anes Altgauzen!</h1>
+    <h1>
         Do Eliseu, nao, hahahaha
     </h1>
     
